f530fb35e5e908e48fab97e3b881011b8d9fc6ef
dean-c8181 <deac.c8181@gmail.com>
2020-11-09 16:45:16 +0900

    Class를 Hooks로 전환하기
    WordRelay.jsx / WordRelay_class.jsx
    
    1. Hooks가 더 간결하다.
    2. React 불러올때 useState, useRef 선언
    3. State선언 바뀜. (this안씀).
    4. 함수는 const로 변수에 담아줘야함.
    5. ref 사용방법 바뀜.(더간단함)

--- lecture2/WordRelay.jsx
+++ lecture2/WordRelay.jsx
@@ -1,61 +1,44 @@
 const React = require('react');
-const {
-    Component
-} = React;
-class WordRelay extends Component { // React.Component 를 const { Component } = React; 선언으로 Component로 줄일 수 있다.
-    state = {
-        word: '김밥',
-        value: '',
-        result: '',
-    };
+const { useState, useRef } = React;
 
-    onSubmitForm = (e) => {
+// class Hooks로 변경
+const WordRelay = () => {
+    const [ word, setWord ] = useState('김밥');
+    const [ value, setValue ] = useState('');
+    const [result, setResult ] = useState('');
+    const inputRef = useRef(null);
+
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-            this.setState({
-                result: '정답!!',
-                word: this.state.value,
-                value: ''
-            });
-            this.input.focus();
+        if (word[word.length - 1] === value[0]) {
+            setResult('정답!!');
+            setWord(value);
+            setValue('');
+            inputRef.current.focus();
         } else {
-            this.setState({
-                result: '땡!',
-                value: ''
-            });
-            this.input.focus();
+            setResult('땡!');
+            setValue('');
+            inputRef.current.focus();
         }
 
     };
 
-    onChangeInput = (e) => {
-        this.setState({
-            value: e.target.value
-        }) // 정확하게하려면 target 대신 currentTarget
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     };
 
-    input;
-
-    onRefInput = (c) => {
-        this.input = c;
-    };
-
-    render() {
-        return ( 
-            <>
-                <div >{this.state.word}</div>
-                <form onSubmit = {this.onSubmitForm} >
-                <input type = "text" ref = {this.onRefInput} value = {this.state.value} onChange = {this.onChangeInput} 
-                    // value와 onchange를 같이(세트로) 넣을게 아니면 defaultValue={this.state.value}를 넣어 줘야한다.
-                />
-                <button> 입력!</button> 
-                </form>
-                <div> {this.state.result}</div> 
-            </>
-        )
-    }
-
+    return ( 
+        <>
+            <div >{word}</div>
+            <form onSubmit = {onSubmitForm} >
+            <input type = "text" ref = {inputRef} value = {value} onChange = {onChangeInput} 
+                // value와 onchange를 같이(세트로) 넣을게 아니면 defaultValue={this.state.value}를 넣어 줘야한다.
+            />
+            <button> 입력!</button> 
+            </form>
+            <div> {result}</div> 
+        </>
+    )
 }
-
 
 module.exports = WordRelay;
