b277633dbe175024ad4ea6875c434c6d7dd23953
dean-c8181 <deac.c8181@gmail.com>
2020-11-11 14:02:28 +0900

    코드보완과 설명추가
    this.state 중복제거하기,
    약간의 설명 추가

--- lecture3/NumberBaseball.jsx
+++ lecture3/NumberBaseball.jsx
@@ -16,36 +16,37 @@ class NumberBaseball extends Component{
     state = {
             result: '',
             value: '',
-            answer: getNumbers(),
+            answer: getNumbers(),       // this를 안쓰는경우 밖으로 뺄수 있고 다른데서도 쓸수있다.
             tries: [],
         };
 
     onSubmitForm = (e) => {
+        const { value, tries, result, answer } = this.state;
         e.preventDefault();
-        if(this.state.value.length == 4){       // 입력한 숫자가 4자리인가? - MaxLength 기능 추가로 사실상 필요없는 조건
-            if(this.state.value === this.state.answer.join('')){        // input으로 들어오는 값은 모두 문자열, join을 거친건 모두 문자열 / join안에 있는 값으로 데이터들을 구분함 ''은 아무런 구분없이 리턴
+        if(value.length == 4){       // 입력한 숫자가 4자리인가? - MaxLength 기능 추가로 사실상 필요없는 조건
+            if(value === answer.join('')){        // input으로 들어오는 값은 모두 문자열, join을 거친건 모두 문자열 / join안에 있는 값으로 데이터들을 구분함 ''은 아무런 구분없이 리턴
                 this.setState({
                     result: '홈런~!',
-                    tries: [...this.state.tries, { try: this.state.value, result: '홈런~!'}],    // react는 push를 사용하면 바뀐것을 인지를 못하기 때문에 배열에 push를 사용하지않고 새로운 배열을 만들어서 예전 배열을 풀어 넣어준 후 바뀌는 것을 처리한다.
+                    tries: [...tries, { try: value, result: '홈런~!'}],    // react는 push를 사용하면 바뀐것을 인지를 못하기 때문에 배열에 push를 사용하지않고 새로운 배열을 만들어서 예전 배열을 풀어 넣어준 후 바뀌는 것을 처리한다.
                 });
                 // 맞춰서 초기화
-                alert(`정답! ${this.state.answer.join('')} 게임을 다시 시작합니다!`);
+                alert(`정답! ${answer.join('')} 게임을 다시 시작합니다!`);
                 this.setState({
                     value:'',
                     answer: getNumbers(),
                     tries: [],
                 });
             }else{
-                const answerArray = this.state.value.split('').map((v) => parseInt(v));     // split(s)의 s를 빈문자열로 지정하면 문자열을 글자단위로 분리. ex.(s,p,l,i,t) 여기선 answer의 배열 값들이 number이기 때문에 input으로 들어온 문자열을 배열화 하고 그 배열의 인자들을 parseInt로 number 속성으로 바꿔주는 작업.
+                const answerArray = value.split('').map((v) => parseInt(v));     // split(s)의 s를 빈문자열로 지정하면 문자열을 글자단위로 분리. ex.(s,p,l,i,t) 여기선 answer의 배열 값들이 number이기 때문에 input으로 들어온 문자열을 배열화 하고 그 배열의 인자들을 parseInt로 number 속성으로 바꿔주는 작업.
                 let strike = 0;
                 let ball = 0;
     
                 // 실패시 초기화 - 10번이상 틀렸을 때
-                if(this.state.tries.length >= 9){
+                if(tries.length >= 9){
                     this.setState({
-                        result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다.`,
+                        result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다.`,
                     });
-                    alert(`10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}였습니다. 게임을 다시 시작합니다!`);
+                    alert(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다. 게임을 다시 시작합니다!`);
                     this.setState({
                         value:'',
                         answer: getNumbers(),
@@ -53,14 +54,14 @@ class NumberBaseball extends Component{
                     });
                 }else{      // 10번 이하로 틀렸을경우
                     for(let i = 0; i < 4; i+= 1){
-                        if(answerArray[i] === this.state.answer[i]) {
+                        if(answerArray[i] === answer[i]) {
                             strike += 1;
-                        }else if(this.state.answer.includes(answerArray[i])){
+                        }else if(answer.includes(answerArray[i])){
                             ball += 1;
                         }
                     }
                     this.setState({
-                        tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
+                        tries: [...tries, {try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
                         value: '',
                     });
                 }
@@ -92,15 +93,16 @@ class NumberBaseball extends Component{
     // 출처: https://cofs.tistory.com/215 [CofS]
 
     render(){
+        const { value, tries, result } = this.state;
         return(
             <>
-                {/* <h1>{this.state.result}</h1>  이게주석 */}
+                <h1>{result}</h1> {/* 이게 주석 */}
                 <form onSubmit={this.onSubmitForm}>
-                    <input type="number" onChange={this.onChangeInput} value={this.state.value} maxLength={4} />
+                    <input type="number" onChange={this.onChangeInput} value={value} maxLength={4} />
                 </form>
-                <div>시도: {this.state.tries.length}</div>
+                <div>시도: {tries.length}</div>
                 <ul>
-                    {this.state.tries.map((y, index) => {
+                    {tries.map((y, index) => {
                         return(
                             <Try key={`${index + 1}차 시도`} tryInfo={y} i={index}/>
                         );
