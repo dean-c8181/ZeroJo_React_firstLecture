ab3abe34fe7248d727cd5c62b77b4dd63762484d
dean-c8181 <deac.c8181@gmail.com>
2020-11-13 11:11:46 +0900

    RSP 초기세팅
    가위바위보 프로젝트 생성

---
+++ lecture5/RSP.jsx
@@ -0,0 +1,15 @@
+import React, { Component, } from 'react';
+
+
+class RSP extends Component{
+
+    render(){
+        return(
+            <>
+                <h1>가위바위보</h1>
+            </>
+        );
+    }
+}
+
+export default RSP;
---
+++ lecture5/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import RSP from './RSP.jsx';
+
+render(<RSP />, document.querySelector('#root'));
